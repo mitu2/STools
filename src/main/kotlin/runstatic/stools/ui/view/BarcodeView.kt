38133bb7e343e5fe331d012e869f67f8fcf1c1cd
mitu2 <chenmoand@outlook.com>
2021-11-23 20:51:03 +0800

    添加BarcodeView

--- src/main/kotlin/runstatic/stools/ui/view/BarcodeView.kt
+++ src/main/kotlin/runstatic/stools/ui/view/BarcodeView.kt
@@ -1,12 +1,22 @@
 package runstatic.stools.ui.view
 
 import com.github.mvysny.karibudsl.v10.*
+import com.github.mvysny.kaributools.addCloseButton
 import com.google.zxing.BarcodeFormat
-import com.vaadin.flow.component.textfield.NumberField
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.vaadin.flow.component.html.Image
+import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.FlexLayout
 import com.vaadin.flow.router.PageTitle
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.server.InputStreamFactory
+import com.vaadin.flow.server.StreamResource
 import com.vaadin.flow.spring.annotation.SpringComponent
 import com.vaadin.flow.spring.annotation.UIScope
+import okhttp3.internal.closeQuietly
+import java.io.ByteArrayOutputStream
 
 
 /**
@@ -14,20 +24,30 @@ import com.vaadin.flow.spring.annotation.UIScope
  * @author chenmoand
  */
 @Route("barcode")
-@PageTitle("二维码生成工具 - static.run")
+@PageTitle("条形码/二维码一键生成 - static.run")
 @UIScope
 @SpringComponent
 class BarcodeView : KComposite() {
 
     private var content = ""
     private var format = BarcodeFormat.QR_CODE
-    private var imageWith = 24
-    private var imageHeight = 24
+    private var imageWith = 240
+    private var imageHeight = 240
 
+    private lateinit var result: Image
+
+    private val contentErr = Notification("请输入内容后再点击制作", 3000, Notification.Position.TOP_CENTER)/*.addCloseButton()*/
 
     private val root = ui {
-        verticalLayout  {
+        flexLayout {
+            flexWrap = FlexLayout.FlexWrap.WRAP
+            justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+            setFlexDirection(FlexLayout.FlexDirection.COLUMN)
+            alignContent = FlexLayout.ContentAlignment.CENTER
+            alignItems = FlexComponent.Alignment.BASELINE
             formLayout {
+                width = "400px"
+                style["margin"] = "0 auto"
                 textField("内容") {
                     value = content
                     isRequired = true
@@ -51,11 +71,33 @@ class BarcodeView : KComposite() {
                     value = imageHeight.toDouble()
                     addValueChangeListener { imageHeight = it.value.toInt() }
                 }
+                button("制作") {
+                    onLeftClick { makeImage() }
+                }
             }
-//            image {
-//
-//            }
+            result = image {
+
+            }
+            add(contentErr)
         }
+
+    }
+
+    fun makeImage() {
+        if(content.isBlank()) {
+            contentErr.open()
+            return
+        }
+        val writer = MultiFormatWriter()
+        val bitMatrix = writer.encode(content, format, imageWith, imageHeight)
+        val outputStream = ByteArrayOutputStream()
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", outputStream)
+        result.setSrc(StreamResource("make.png", InputStreamFactory {
+            return@InputStreamFactory outputStream.toByteArray().inputStream()
+        }))
+        result.width = "${imageHeight}px"
+        result.height = "${imageWith}px"
+        outputStream.closeQuietly()
     }
 
 }
--- src/main/kotlin/runstatic/stools/util/VaadinDsl.kt
+++ src/main/kotlin/runstatic/stools/util/VaadinDsl.kt
@@ -7,3 +7,5 @@ import com.vaadin.flow.data.binder.Binder
  * @author chenmoand
  */
 inline fun <reified BEAN> BEAN.binder() = Binder(BEAN::class.java)
+
+
