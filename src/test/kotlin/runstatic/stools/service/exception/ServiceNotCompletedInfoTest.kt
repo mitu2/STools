a7eb736acbc92b2e6af0b50c9c09d70f54acb78e
chenmoand <chenmoand@outlook.com>
2022-06-28 20:58:33 +0800

    update SecurityConfiguration new

--- src/test/kotlin/runstatic/stools/service/exception/ServiceNotCompletedInfoTest.kt
+++ src/test/kotlin/runstatic/stools/service/exception/ServiceNotCompletedInfoTest.kt
@@ -1,18 +1,10 @@
 package runstatic.stools.service.exception
 
-import org.junit.jupiter.api.Test
-
 /**
  *
  * @author chenmoand
  */
-class ServiceNotCompletedInfoTest {
+internal class ServiceNotCompletedInfoTest {
 
-    @Test
-    fun mapTest() {
-        val map = hashMapOf<String, String>()
-        map["name"] = "abc"
-        assert(map.size > 0)
-    }
 
 }
