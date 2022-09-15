05979d185adaf50e50715bf04794e2d37f409dcd
chenmoand <chenmoand@outlook.com>
2022-09-15 20:49:27 +0800

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,9 @@ plugins {
     id("org.springframework.boot") version "2.7.0"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("com.vaadin") version "23.2.0"
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.spring") version "1.5.31"
-    kotlin("plugin.jpa") version "1.5.31"
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+    kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "runstatic"
@@ -30,14 +30,14 @@ repositories {
 }
 
 ext {
-    this["vaadinVersion"] = "23.2.0"
-    this["log4j2.version"] = "2.17.1"
-    this["zxingVersion"] = "3.5.0"
+    this["vaadin.version"] = "23.2.0"
+    this["log4j2.version"] = "2.18.0"
+    this["zxing.version"] = "3.5.0"
 }
 
 
 dependencies {
-    implementation(platform("org.apache.logging.log4j:log4j-bom:2.17.1"))
+    implementation(platform("org.apache.logging.log4j:log4j-bom:${property("log4j2.version")}"))
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
@@ -48,7 +48,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.session:spring-session-core")
-    implementation("com.vaadin:vaadin-spring-boot-starter:${property("vaadinVersion")}")
+    implementation("com.vaadin:vaadin-spring-boot-starter:${property("vaadin.version")}")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("mysql:mysql-connector-java:8.0.29")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -58,9 +58,9 @@ dependencies {
     // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     // https://mvnrepository.com/artifact/com.google.zxing/core
-    implementation("com.google.zxing:core:${property("zxingVersion")}")
+    implementation("com.google.zxing:core:${property("zxing.version")}")
     // https://mvnrepository.com/artifact/com.google.zxing/javase
-    implementation("com.google.zxing:javase:${property("zxingVersion")}")
+    implementation("com.google.zxing:javase:${property("zxing.version")}")
 //    implementation("io.springfox:springfox-boot-starter:3.0.0")
     // https://mvnrepository.com/artifact/com.github.mvysny.karibudsl/karibu-dsl-v23
     implementation("com.github.mvysny.karibudsl:karibu-dsl:1.1.3")
@@ -70,13 +70,13 @@ dependencies {
 
 dependencyManagement {
     imports {
-        mavenBom("com.vaadin:vaadin-bom:${property("vaadinVersion")}")
+        mavenBom("com.vaadin:vaadin-bom:${property("vaadin.version")}")
     }
 }
 configurations.all {
     resolutionStrategy.eachDependency {
         if (requested.group == "org.apache.logging.log4j") {
-            useVersion("2.17.1")
+            useVersion(property("log4j2.version") as String)
         }
     }
 }
